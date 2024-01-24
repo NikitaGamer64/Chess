a681ec8de1252f4528d51f82c40683dc6bb9bae7
NikitaGamer64 <nikitakordonov@mail.ru>
2024-01-24 23:53:50 +0300

    Конфигурация доски

---
+++ ChessLogic/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class GameState(Player player, Board board)
+    {
+        public Board Board { get; } = board;
+        //Конфигурация доски
+        public Player CurrentPlayer { get; private set; } = player;
+        //Чей ход
+    }
+}
--- ChessLogic/Pieces/King.cs
+++ ChessLogic/Pieces/King.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     public class King(Player color) : Piece
     //Код для короля
