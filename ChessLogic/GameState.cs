5cc1c227f367bf148669b0af587eea6856230cdb
NikitaGamer64 <nikitakordonov@mail.ru>
2024-01-28 20:41:05 +0300

    Мат и пат

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -12,6 +12,7 @@ namespace ChessLogic
         public Board Board { get; } = board;
         //Конфигурация доски
         public Player CurrentPlayer { get; private set; } = player;
+        public Result Result { get; private set; } = null;
         //Чей ход
 
         public IEnumerable<Move> LegalMovesForPiece(Position pos)
@@ -28,6 +29,36 @@ namespace ChessLogic
         {
             move.Execute(Board);
             CurrentPlayer = CurrentPlayer.Opponent();
+            CheckForGameOver();
+        }
+
+        public IEnumerable<Move> AllLegalMovesFor(Player player)
+        {
+            IEnumerable<Move> moveCandidates = 
+                Board.PiecePositionsFor(player).SelectMany(pos =>
+                {
+                    Piece piece = Board[pos];
+                    return piece.GetMoves(pos, Board);
+                });
+            return moveCandidates.Where(move => move.IsLegal(Board));
+        }
+        private void CheckForGameOver()
+        {
+            if (!AllLegalMovesFor(CurrentPlayer).Any())
+            {
+                if (Board.IsInCheck(CurrentPlayer))
+                {
+                    Result = Result.Win(CurrentPlayer.Opponent());
+                }
+                else
+                {
+                    Result = Result.Draw(EndReason.Stalemate);
+                }
+            }
+        }
+        public bool IsGameOver()
+        {
+            return Result != null;
         }
     }
 }
