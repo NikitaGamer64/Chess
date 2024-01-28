e8fc13eb85531277c47536a468529cfcf4401328
NikitaGamer64 <nikitakordonov@mail.ru>
2024-01-28 21:56:54 +0300

    Конец игры: мат и пат

--- ChessUI/GameOverMenu.xaml.cs
+++ ChessUI/GameOverMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,24 +16,57 @@ using System.Windows.Shapes;
 
 namespace ChessUI
 {
-    /// <summary>
-    /// Логика взаимодействия для GameOverMenu.xaml
-    /// </summary>
     public partial class GameOverMenu : UserControl
     {
-        public GameOverMenu()
+        public event Action<Option> OptionSelected;
+        public GameOverMenu(GameState gameState)
         {
             InitializeComponent();
+            Result result = gameState.Result;
+            WinnerText.Text = GetWinnerText(result.Winner);
+            ReasonText.Text = GetReasonText(result.Reason, gameState.CurrentPlayer);
         }
+
+        private static string GetWinnerText(Player winner)
+        {
+            return winner switch
+            {
+                Player.White => "Победа белых",
+                Player.Black => "Победа чёрных",
+                _ => "Ничья"
+            };
+        }
+        private static string PlayerString(Player player)
+        {
+            return player switch
+            {
+                Player.White => "белым",
+                Player.Black => "чёрным",
+                _ => ""
+            };
+        }
+        private static string GetReasonText(EndReason reason, Player currentPlayer)
+        {
+            return reason switch
+            {
+                EndReason.Stalemate => $"Пат {PlayerString(currentPlayer)}",
+                EndReason.Checkmate => $"Мат {PlayerString(currentPlayer)}",
+                EndReason.FiftyMoveRule => "50 беспрогрессивных ходов",
+                EndReason.InsufficientMaterial => "Недостаточно фигур",
+                EndReason.ThreefoldRepetition=> "Позиция повторилась 3 раза",
+                _ => ""
+            };
+        }
+
         private void Restart_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Restart);
         }
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Exit);
         }
 
-        
+
     }
 }
