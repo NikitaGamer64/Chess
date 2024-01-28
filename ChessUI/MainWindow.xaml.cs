b5644837e41f068e25aff55b0433f303751636aa
NikitaGamer64 <nikitakordonov@mail.ru>
2024-01-28 19:37:36 +0300

    Оживление системы ходов

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -15,13 +15,18 @@ namespace ChessUI
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+        private readonly Rectangle[,] highLights = new Rectangle[8, 8];
+        private readonly Dictionary<Position, Move> moveCache = [];
+
         private GameState gameState;
+        private Position selectedPos = null;
         public MainWindow()
         {
             InitializeComponent();
             InitializeBoard();
             gameState = new GameState(Player.White, Board.Initial());
             DrawBoard(gameState.Board);
+            SetCursor(gameState.CurrentPlayer);
         }
         private void InitializeBoard()
         {
@@ -32,6 +37,9 @@ namespace ChessUI
                     Image image = new();
                     pieceImages[r, c] = image;
                     PieceGrid.Children.Add(image);
+
+                    Rectangle highlight = new();
+                    highLights[r, c] = highlight;
                 }
             }
         }
@@ -44,6 +52,90 @@ namespace ChessUI
                     Piece piece = board[r, c];
                     pieceImages[r, c].Source = Images.GetImage(piece);
                 }
+            }
+        }
+
+        private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point point = e.GetPosition(BoardGrid);
+            Position pos = ToSquarePosition(point);
+            if (selectedPos == null)
+            {
+                OnFromPositionSelected(pos);
+            }
+            else
+            {
+                OnToPositionSelected(pos);
+            }
+        }
+        private Position ToSquarePosition(Point point)
+        {
+            double SquareSize = BoardGrid.ActualWidth / 8;
+            int row = (int)(point.Y / SquareSize);
+            int col = (int)(point.X / SquareSize);
+            return new Position(row, col);
+        }
+
+        private void OnFromPositionSelected(Position pos)
+        {
+            IEnumerable<Move> moves = gameState.LegalMovesForPiece(pos);
+            if (moves.Any())
+            {
+                selectedPos = pos;
+                CacheMoves(moves);
+                ShowHighlights();
+            }
+        }
+        private void OnToPositionSelected(Position pos)
+        {
+            selectedPos = null;
+            HideHighlights();
+
+            if (moveCache.TryGetValue(pos, out Move move))
+            {
+                HandleMove(move);
+            }
+        }
+        private void HandleMove(Move move)
+        {
+            gameState.MakeMove(move);
+            DrawBoard(gameState.Board);
+            SetCursor(gameState.CurrentPlayer);
+        }
+
+        private void CacheMoves(IEnumerable<Move> moves)
+        {
+            moveCache.Clear();
+            foreach (Move move in moves)
+            {
+                moveCache[move.ToPos] = move;
+            }
+        }
+        private void ShowHighlights()
+        {
+            Color color = Color.FromArgb(150, 125, 255, 125);
+
+            foreach (Position to in moveCache.Keys)
+            {
+                highLights[to.Row, to.Column].Fill = new SolidColorBrush(color);
+            }
+        }
+        private void HideHighlights()
+        {
+            foreach (Position to in moveCache.Keys)
+            {
+                highLights[to.Row, to.Column].Fill = Brushes.Transparent;
+            }
+        }
+        private void SetCursor(Player player)
+        {
+            if (player == Player.White)
+            {
+                Cursor = ChessCursors.WhiteCursor;
+            }
+            else
+            {
+                Cursor = ChessCursors.BlackCursor;
             }
         }
     }
